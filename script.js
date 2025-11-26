23b859176a5abf29cd643594332af5cf993bf756
Edgar <edgarwow1991@gmail.com>
2025-11-26 09:01:17 -0600

    buses

--- script.js
+++ script.js
@@ -56,8 +56,24 @@ function changeToSpanish() {
             <p class="center">Con todo nuestro amor,</p>
             <p class="center">Edgar & Aracelis</p>
 
+           <p>P.S.:El personal del Hotel los contactará para efectos de reservación y pago de su estadía</p>
             <p>
-                P.D.: No olvides visitar el resto del sitio web para obtener todos los detalles: horario, ubicación, alojamiento y más. ¡Avísanos si necesitas algo!
+                Para quienes no tengan transporte al lugar de la boda, tendremos un autobús que saldrá del Hotel Sheraton Reforma a las 4 p. m. el viernes 16 de Enero.
+            </p>
+            <p>Si le interesa reservar un asiento en este autobús, por favor confirme su asiento enviándome un mensaje de WhatsApp al +52 18804189, especificando cuántos asientos desea y el nombre de las personas, junto con su comprobante de pago (detalles de pago en la pestaña "Regalos y Contribuciones") o avíseme si prefiere pagar en efectivo en el lugar. Cada asiento cuesta $25 USD / $500 MXN. Esto cubrirá su viaje de ida y vuelta a la CDMX.</p>
+             <div class="mapita">
+                <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3762.6918826523006!2d-99.16827294896557!3d19.425714404798086!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d1ff3591c1ac8f%3A0xbc2d97a9f990a00f!2sSheraton%20Maria%20Isabel%20Mexico%20City%20Reforma!5e0!3m2!1sen!2smx!4v1763843090416!5m2!1sen!2smx"
+                    width="600"
+                    height="450"
+                    style="border: 0"
+                    allowfullscreen=""
+                    loading="lazy"
+                    referrerpolicy="no-referrer-when-downgrade"
+                ></iframe>
+            </div>
+            <p>
+                No olvides visitar el resto del sitio web para obtener todos los detalles: horario, ubicación, alojamiento y más. ¡Avísanos si necesitas algo!
             </p>
     `;
 }
@@ -90,9 +106,31 @@ function changeToEnglish() {
             <p class="center">With all our love,</p>
             <p class="center">Edgar & Aracelis</p>
 
+             <p>P.S.: Hotel staff will be contacting you for payment and reservation of your room/vila through WhatsApp</p>
             <p>
-                P.S. Don’t forget to check out the rest of the site for all the details—schedule, location, accommodations, and more. Let us
-                know if you need anything!
+                For people that don't have transportation to the wedding venue, we are going to have a bus departing from Hotel Sheraton
+                Reforma at 4PM on Friday 16th
+            </p>
+            <p>
+                In case you are interested on having a seat on this bus, please confirm your seat by sending me a whatsapp message to +52
+                18804189 specifying how many seats would you like, and name of those people with your proof of payment (Payment details on
+                the Gifts & Contributions tab) or let me know if you would like to pay with cash at the venue . Each sit costs $25 USD /
+                $500 MXN. This will cover your departure and return to CDMX
+            </p>
+             <div class="mapita">
+                <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3762.6918826523006!2d-99.16827294896557!3d19.425714404798086!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d1ff3591c1ac8f%3A0xbc2d97a9f990a00f!2sSheraton%20Maria%20Isabel%20Mexico%20City%20Reforma!5e0!3m2!1sen!2smx!4v1763843090416!5m2!1sen!2smx"
+                    width="600"
+                    height="450"
+                    style="border: 0"
+                    allowfullscreen=""
+                    loading="lazy"
+                    referrerpolicy="no-referrer-when-downgrade"
+                ></iframe>
+            </div>
+            <p>
+                Don’t forget to check out the rest of the site for all the details—schedule, location, accommodations, and more. Let us know
+                if you need anything!
             </p>
     `;
 }
